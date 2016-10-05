ac4396a61191a404216c9f6d575d4c131df0c357
jaredmichaelsmith <jared@jaredsmith.io>
2016-10-05 12:53:19 -0400

    Add comments

--- cidr_to_asn/src/main.rs
+++ cidr_to_asn/src/main.rs
@@ -1,3 +1,12 @@
+//  cidr_to_asn
+//  usage: cidr_to_asn --help
+//
+//  Versio: 0.1.0
+//  Author: Jared M. Smith <jared@jaredsmith.io>
+//
+//
+
+
 extern crate clap;
 
 use std::io::BufReader;
@@ -10,7 +19,9 @@ use std::collections::HashMap;
 use clap::{Arg, App};
 
 fn main() {
+    // The main function of the program
 
+    // Parse CLI arguments
     let matches = App::new("cidr_to_asn")
         .version("0.1")
         .author("Jared M. Smith <jared@jaredsmith.io>")
@@ -30,7 +41,7 @@ fn main() {
 
     let input_filename = matches.value_of("INPUT").unwrap();
 
-    // println!("Reading in RIB data...");
+    // Open RIB file for reading
     let f = match File::open(input_filename) {
         Ok(file) => file,
         Err(e) => {
@@ -40,23 +51,30 @@ fn main() {
         }
     };
 
+    // Define the hashmap for mapping CIDR's to AS #'s
     let mut mappings: HashMap<String, Vec<String>> = HashMap::new();
-
     let file = BufReader::new(&f);
+
     for line in file.lines() {
+
+        // Break up each line
         let l = line.unwrap();
-        // println!("{}", l);
         let entry: Vec<&str> = l.split('|').collect();
 
+        // Extract the CIDR and the AS level path
         let cidr = entry[5].to_string();
         let asn_nums_str = entry[6].to_string();
 
+        // Break up the AS level path
         let asn_nums: Vec<&str> = asn_nums_str.split(' ').collect();
         if asn_nums.is_empty() {
             continue;
         }
 
+        // Get the last AS number in the path
         let last_asn_num = asn_nums.last().unwrap();
+
+        // Build the mappings from CIDR's to the last AS number in each path
         if mappings.contains_key(&cidr) {
             let mut existing_asn_nums = mappings.get_mut(&cidr).unwrap();
             existing_asn_nums.push(last_asn_num.to_string());
@@ -67,13 +85,18 @@ fn main() {
         }
     }
 
+    // Open a file for writing the final results
     let output_filename = matches.value_of("OUTPUT").unwrap();
-
     let mut buffer = File::create(output_filename).unwrap();
 
+    // Go through each mapping, find the most common AS number among the different possibilities,
+    // and build another mapping to be written to a file
+    // We have to do this because AS's could lie about their routes, so we choose the most common
+    // AS number and assume that is the true route
     for (cidr, as_nums) in mappings {
-        // println!("ASN's for {}", cidr);
 
+        // Build a mapping of AS nums to the number of times they show up
+        // In Python this would be super easy, but I don't know a better way to do this in Rust yet
         let mut asn_counts: HashMap<String, i32> = HashMap::new();
         for as_num in as_nums {
             if asn_counts.contains_key(&as_num) {
@@ -84,12 +107,11 @@ fn main() {
             }
         }
 
-
         let mut most_common_as_num: String = String::new();
         let mut last_count: i32 = 0;
 
+        // Get the most common AS
         for (as_num, count) in asn_counts {
-            // println!("as_num {} occurs {} times", as_num, count);
 
             if count > last_count {
                 most_common_as_num = as_num;
@@ -97,11 +119,7 @@ fn main() {
             }
         }
 
+        // Write the results to a file in CSV format
         write!(&mut buffer, "{},{}\n", cidr, most_common_as_num);
-
-        // println!("Most Common AS Num for CIDR {}: {}",
-        // cidr,
-        // most_common_as_num);
-        //
     }
 }
